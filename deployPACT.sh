03498242f4ee2b439f93b55e9076a664dfb124a7
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2022-10-08 20:06:21 +0530

    Update and rename deployEnv.sh to deployPACT.sh

--- deployPACT.sh
+++ deployPACT.sh
@@ -1,4 +1,3 @@
 sudo apt update && sudo apt install default-jdk -y && sudo apt install maven -y && sudo apt install docker.io docker-compose -y
 sudo docker-compose -f "pact-broker/docker-compose.yml" up -d --build
-cd start/inventory
-mvn liberty:dev
+
