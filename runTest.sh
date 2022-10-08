03bb7a1ca21f2a37e62dce295168aad4353f05d9
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2022-10-08 20:12:22 +0530

    Create runTest.sh

---
+++ runTest.sh
@@ -0,0 +1,2 @@
+sudo mv contract-testing/InventoryPactIT.java contract-testing/start/inventory/src/test/java/io/openliberty/guides/inventory/InventoryPactIT.java
+sudo mv contract-testing/pom.xml contract-testing/start/inventory/pom.xml
